ba88054e3279399b3bc218e42dcb2d04de86b03d
Katy Daviscourt and Tammy Dang <katy+tammy@>
2017-04-18 11:06:55 -0700

    add edit and delete functions

--- src/WebApplication1/Controllers/TeamsController.cs
+++ src/WebApplication1/Controllers/TeamsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sports.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,6 +35,35 @@ namespace Sports.Controllers
             db.Teams.Add(team);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams .TeamId == id);
+            return View(thisTeam); 
+        }
+
+        [HttpPost]
+        public IActionResult Edit (Team team)
+        {
+            db.Entry(team).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams .TeamId == id);
+            return View(thisTeam);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
+            db.Teams.Remove(thisTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index"); 
         }
     }
 }
